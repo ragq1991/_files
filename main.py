3cd281a16ab3a6d0232415888e08f7e523246a5f
ragq <ragq1991@gmail.com>
2021-03-24 19:02:00 +0600

    done

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
+from pprint import pprint
+
 def pretty(d):
     print('cook_book = {')
     for key, value in d.items():
-        print("'", str(key), "': [", sep = '')
+        print("'", str(key), "': [", sep='')
         for item in value:
             print('\t', item)
         print('\t', '],')
     print('}')
 
 def get_cook_book():
-    glossary = {}
     with open('text.txt', 'r', encoding="utf-8") as f:
         glossary = {}
         for line in f:
             if line.find('|') > 0:
                 ingredient_name = line[0:line.find('|') - 1]
                 ingredient_quantity = line[line.find('|') + 2:line.rfind('|') - 1]
-                if line[len(line)-1] == chr(10):
-                    measure = line[line.rfind('|') + 2:len(line)-1]
+                if line[len(line) - 1] == chr(10):
+                    measure = line[line.rfind('|') + 2:len(line) - 1]
                 else:
                     measure = line[line.rfind('|') + 2:len(line)]
-                second_glossary = {}
-                second_glossary['ingredient_name'] = ingredient_name
-                second_glossary['ingredient_quantity'] = ingredient_quantity
-                second_glossary['measure'] = measure
+                second_glossary = {'ingredient_name': ingredient_name, 'ingredient_quantity': ingredient_quantity,
+                                   'measure': measure}
                 empty_list.append(second_glossary)
                 glossary[name] = empty_list
                 # ингридиент
@@ -35,9 +34,9 @@ def get_cook_book():
                 # print('пустая строка')
                 # пустая строка
             else:
-                name = line[0:len(line)-1]
+                name = line[0:len(line) - 1]
                 empty_list = []
-    pretty(glossary)
+    # pretty(glossary)
     return glossary
 
 def get_shop_list_by_dishes(dishes, person_count):
@@ -48,12 +47,14 @@ def get_shop_list_by_dishes(dishes, person_count):
         if cook in glossary.keys():
             for ingredient in glossary.get(cook):
                 if ingredient.get('ingredient_name') in ingredients.keys():
-                    count_1 = ingredient.get('ingredient_quantity')
-                    vremennii = ingredients.get(ingredient.get('ingredient_name'))
-                    count_2 = vremennii.get('quantity')
-                    total_count = ingredients.get(ingredient.get('quantity')) + glossary.get(cook.get('ingredient_quantity'))
-            ingredients = glossary.get(cook)
+                    ready = ingredients[ingredient.get('ingredient_name')]
+                    ready['measure'] = ingredient.get('measure')
+                    count = person_count * int(ingredient.get('ingredient_quantity'))
+                    ready['quantity'] += count
+                else:
+                    ready['measure'] = ingredient.get('measure')
+                    ready['quantity'] = int(ingredient.get('ingredient_quantity')) * person_count
+                ingredients[ingredient.get('ingredient_name')] = ready.copy()
+    return ingredients
 
-
-
-get_shop_list_by_dishes(['Фахитос', 'Запеченный картофель'], 0)
+pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 10))
